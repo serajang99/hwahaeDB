c04bc1f22c87ba96c312a7b8ec7d90cb0b7730c7
student <att815@naver.com>
2023-08-03 15:49:27 +0900

    listaddComment

--- src/pureView/pureViewUi.java
+++ src/pureView/pureViewUi.java
@@ -1,15 +1,27 @@
 package pureView;
 
+import java.util.List;
 import java.util.Scanner;
+
+import pureView.dao.BoardException;
+import pureView.dto.BoardDto;
+import pureView.service.BoardService;
+import pureView.service.BoardServiceImpl;
 
 
 public class pureViewUi {
 	private Scanner sc;
-
+	private BoardService brdSvc;
+	
 	public static void main(String[] args) {
 		new pureViewUi().go();
 	}
-
+	
+	private void init() {
+		sc = new Scanner(System.in);
+		brdSvc = new BoardServiceImpl();
+	}
+	
 	private void go() {
 		init();
 		while (true)
@@ -34,15 +46,15 @@ public class pureViewUi {
 		} else if (menu == 4) {
 			
 		} else if (menu == 5) {
-			
+			listBoard();
 		} else if (menu == 6) {
-			
+			addBoard();
 		} else if (menu == 7) {
 			
 		} else if (menu == 8) {
-			
+//			updateBoard();
 		} else if (menu == 9) {
-			
+//			deleteBoard();
 		} else if (menu == 10) {
 			
 		} else if (menu == 11) {
@@ -50,11 +62,48 @@ public class pureViewUi {
 		} else if (menu == 12) {
 			
 		} 
-		
-		
+	}
+	
+	private void addBoard() {
+		System.out.println("** 게시물 등록 **");
+		System.out.println("제목을 입력하세요>>");
+		String title = sc.nextLine();
+		System.out.println("아이디를 입력하세요>>");
+		String memberId = sc.nextLine();
+		System.out.println("내용을 입력하세요>>");
+		String BoardContent = sc.nextLine();
+		System.out.println("별점을 매겨주세요 (1~5점)>>");
+		int starRating = Integer.parseInt(sc.nextLine());
+		System.out.println("화장품 번호를 입력해주세요>>");
+		int cosNum = Integer.parseInt(sc.nextLine());
+		BoardDto dto = new BoardDto(0, title, BoardContent, null, starRating, memberId, cosNum);
+		try {
+			brdSvc.add(dto);
+		} catch (BoardException e) {
+			e.printStackTrace();
+			System.out.println("게시물 등록 오류");
+		}
 	}
 
-	private void init() {
-		sc = new Scanner(System.in);
+	private void listBoard() {
+		System.out.println("[게시물 목록]");
+		List<BoardDto> list = null;
+		
+		try {
+			list = brdSvc.list();
+			for (BoardDto dto : list) {
+				System.out.println(
+						dto.getBoardNum()+ "      " + 
+						dto.getBoardTitle() + "      " + 
+						dto.getBoardContent() + "      " + 
+						dto.getWriteTime() + "      " +
+						dto.getStarRating() + "      " +
+						dto.getMemberId() + "      " +
+						dto.getCosNum());
+			}
+		} catch (BoardException e) {
+			System.out.println("*** 서버에 오류가 발생 ***");
+		}
 	}
+	
 }
