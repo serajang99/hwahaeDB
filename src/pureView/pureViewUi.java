e1347a5b877fbb122eff2221bbfe4e3cf97f0f09
student <serajang1679@gmail.com>
2023-08-03 13:23:12 +0900

    fix: 메뉴 수정

--- src/pureView/pureViewUi.java
+++ src/pureView/pureViewUi.java
@@ -17,14 +17,15 @@ public class pureViewUi {
 	}
 	
 	private void mainMenu() {
-		System.out.println("메인 메뉴: (0) 종료 (1)로그인 (2)로그아웃 "
+		System.out.println("메인 메뉴: (0) 회원가입 (1)로그인 (2)로그아웃 "
 				+ "(3)화장품 목록 (4)화장품 상세보기 "
 				+ "(5)리뷰 게시판 목록 (6)리뷰 등록 (7)리뷰 상세보기(댓글 포함) (8)리뷰 수정 (9)리뷰 삭제 "
-				+ "(10)댓글 등록 (11)댓글 수정 (12)댓글 삭제");
+				+ "(10)댓글 등록 (11)댓글 수정 (12)댓글 삭제"
+				+ "(13)종료");
 		System.out.println("메뉴 선택: ");
 		int menu = Integer.parseInt(sc.nextLine());
 		if (menu == 0) {
-			System.exit(0);
+			
 		} else if (menu == 1) {
 //			login();
 		} else if (menu == 2) {
@@ -49,7 +50,9 @@ public class pureViewUi {
 			
 		} else if (menu == 12) {
 			
-		} 
+		} else {
+			System.exit(0);
+		}
 		
 		
 	}
