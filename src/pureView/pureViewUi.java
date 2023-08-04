c6ed3801b0fc1f398bb10561e83cafa2b65f592a
serajang99 <serajang1679@gmail.com>
2023-08-04 10:49:53 +0900

    style: 코드 수정

--- src/pureView/pureViewUi.java
+++ src/pureView/pureViewUi.java
@@ -31,7 +31,7 @@ public class pureViewUi {
 	private CommentService cmtSvc; // 내용 서비스
 	private MemberService mbSvc; // 회원 서비스
 	private LogService logSvc; // 로그 서비스
-  	private CosmeticService csmtSvc; // 화장품서비스
+	private CosmeticService csmtSvc; // 화장품서비스
 
 	public static void main(String[] args) {
 		new pureViewUi().go();
@@ -41,8 +41,8 @@ public class pureViewUi {
 		sc = new Scanner(System.in);
 		brdSvc = new BoardServiceImpl();
 		cmtSvc = new CommentServiceImpl();
-	    csmtSvc = new CosmeticServiceImpl();
-	    mbSvc = new MemberServiceImpl();
+		csmtSvc = new CosmeticServiceImpl();
+		mbSvc = new MemberServiceImpl();
 		logSvc = new LogServiceImpl();
 	}
 
@@ -62,17 +62,13 @@ public class pureViewUi {
 			System.out.println("원하시는 기능을 선택하세요 : (0) 회원 가입 (1)회원 정보 수정 (2)회원 삭제 ");
 			System.out.println("기능 선택 : ");
 			int func = Integer.parseInt(sc.nextLine());
-			if(func == 0) {
-				// 회원 가입
+			if (func == 0) {
 				addMember();
-			}
-			  else if (func == 1) {
-//				// 회원 정보 수정
+			} else if (func == 1) {
 				updateMember();
 			} else if (func == 2) {
-//				// 회원 삭제
 				deleteMember();
-			} 
+			}
 		} else if (menu == 1) {
 			addLog();
 		} else if (menu == 2) {
@@ -102,42 +98,38 @@ public class pureViewUi {
 		}
 	}
 
-
 	private void updateLog() {
 		System.out.println("** 로그아웃 **");
 		System.out.println("아이디를 입력하세요 : ");
-		String id = sc.nextLine();		
-		
-		LoginDto dto = new LoginDto(0, null, null, id); 
+		String id = sc.nextLine();
+
+		LoginDto dto = new LoginDto(0, null, null, id);
 
 		try {
 			try {
 				logSvc.update(dto);
 				System.out.println("로그아웃 되었습니다.");
 			} catch (RecordNotFoundException e) {
-				
+
 			}
 		} catch (LogException e) {
-			// TODO Auto-generated catch block
 			System.out.println("로그아웃 오류");
 		}
 	}
 
-
 	private void addLog() {
 		System.out.println("** 로그인 **");
 		System.out.println("아이디를 입력하세요 : ");
-		String id = sc.nextLine();		
-		
+		String id = sc.nextLine();
+
 		LoginDto dto = new LoginDto(0, null, null, id);
 		System.out.println("비밀번호를 입력하세요 : ");
 		String pw = sc.nextLine();
 		System.out.println("로그인이 되셨습니다!");
-		
+
 		try {
 			logSvc.add(dto);
 		} catch (LogException e) {
-			// TODO Auto-generated catch block
 			System.out.println("로그인 오류");
 		}
 
@@ -146,19 +138,19 @@ public class pureViewUi {
 	private void cosmeticsList() {
 		System.out.println("[화장품 목록]");
 		List<CosmeticDto> list;
-		
+
 		try {
 			System.out.println("보고 싶은 제품군을 번호로 작성해주세요");
-			String[] category = {"스킨/토너", "세럼/앰플", "로션/크림", "전체"};
+			String[] category = { "스킨/토너", "세럼/앰플", "로션/크림", "전체" };
 			System.out.println("(1)스킨/토너 (2)세럼/앰플 (3)로션/크림 (4)전체");
-			String c = category[Integer.parseInt(sc.nextLine())-1];
-			
+			String c = category[Integer.parseInt(sc.nextLine()) - 1];
+
 			System.out.println("정렬 기준을 번호로 작성해주세요");
 			System.out.println("(1)이름 (2)가격 (3)용량");
-			String[] orderBy = {"cosnum", "price", "volume"};
-			String ob = orderBy[Integer.parseInt(sc.nextLine())-1];
-			
-			list = csmtSvc.list(c, ob);			
+			String[] orderBy = { "cosnum", "price", "volume" };
+			String ob = orderBy[Integer.parseInt(sc.nextLine()) - 1];
+
+			list = csmtSvc.list(c, ob);
 			for (CosmeticDto dto : list) {
 				System.out.println(dto);
 			}
@@ -166,7 +158,7 @@ public class pureViewUi {
 			System.out.println("잘못된 입력입니다");
 		}
 	}
-	
+
 	private void cosmeticsDetail() {
 		System.out.println("성분이 궁금하신 화장품 이름을 작성해주세요");
 		CosmeticDto result;
@@ -175,24 +167,24 @@ public class pureViewUi {
 		if (result != null)
 			System.out.println(result);
 	}
-	
+
 	private void listDetailBoard() {
 		List<CommentDto> commentList = null;
 		System.out.println("리뷰를 볼 게시물 번호를 입력하세요>> ");
 		int BoardNum = Integer.parseInt(sc.nextLine());
-		
+
 		try {
 			BoardDto dto = brdSvc.read(BoardNum);
 			System.out.println("** 게시판 상세보기 **");
 			System.out.println(dto.getBoardNum() + "      " + dto.getBoardTitle() + "      " + dto.getBoardContent()
-			+ "      " + dto.getWriteTime() + "      " + dto.getStarRating() + "      " + dto.getMemberId()
-			+ "      " + dto.getCosNum());
+					+ "      " + dto.getWriteTime() + "      " + dto.getStarRating() + "      " + dto.getMemberId()
+					+ "      " + dto.getCosNum());
 		} catch (BoardException e) {
 			System.out.println("---게시판 서버 오류---");
 		} catch (RecordNotFoundException e) {
 			System.out.println("없는 게시물입니다.");
 		}
-		
+
 		try {
 			commentList = cmtSvc.list(BoardNum);
 			for (CommentDto dto : commentList) {
@@ -202,9 +194,8 @@ public class pureViewUi {
 		} catch (BoardException e) {
 			System.out.println("*** 서버에 오류가 발생 ***");
 		}
-		
-	}	
-		
+
+	}
 
 	private void addMember() {
 		System.out.println("** 회원 가입 **");
@@ -217,10 +208,10 @@ public class pureViewUi {
 		System.out.println("나이를 입력하세요 : ");
 		int age = Integer.parseInt(sc.nextLine());
 		String skin_type = "";
-		while(true) {
+		while (true) {
 			System.out.println("피부 타입을 입력하세요 (건성, 지성, 복합성 中 1) : ");
 			skin_type = sc.nextLine();
-			if("건성".equals(skin_type) || "지성".equals(skin_type) || "복합성".equals(skin_type)) {
+			if ("건성".equals(skin_type) || "지성".equals(skin_type) || "복합성".equals(skin_type)) {
 				break;
 			}
 		}
@@ -233,67 +224,54 @@ public class pureViewUi {
 		}
 	}
 
-	
 	private void updateMember() {
 		System.out.println("수정할 회원 정보의 아이디를 입력하세요>> ");
 		String id = (sc.nextLine());
 		MemberDto dto = null;
-			try {
-				dto = mbSvc.findById(id);
-				System.out.println(mbSvc.findById(id));
-				System.out.println("** 상세보기 **");
-//				System.out.println("수정할 아이디 (없으면 엔터) : ");
-//				String id_2 = sc.nextLine();
-//				id_2 = id_2.length()==0?dto.getId():id_2;
-				System.out.println("수정할 이름 (없으면 엔터): ");
-				String name_2 = sc.nextLine();
-				name_2 = name_2.length()==0?dto.getName():name_2;
-				System.out.println("수정할 비밀번호 (없으면 엔터): ");
-				String passwd_2 = sc.nextLine();
-				passwd_2 = passwd_2.length()==0?dto.getPasswd():passwd_2;
-				System.out.println("수정할 피부타입 (없으면 엔터) : ");
-				String skin_type_2 = sc.nextLine();
-				skin_type_2 =skin_type_2.length()==0?dto.getSkintype():skin_type_2;
-				System.out.println("수정할 나이 (없으면 엔터) : ");
-				String age_2 = sc.nextLine();
-				int age_3=0;
-				age_3 = age_2.length()==0?dto.getAge():Integer.parseInt(age_2);
-				dto.setId(id);
-				dto.setName(name_2);
-				dto.setPasswd(passwd_2);
-				dto.setSkintype(age_2);
-				dto.setAge(age_3);
-				mbSvc.update(dto);
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (MemberException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
+		try {
+			dto = mbSvc.findById(id);
+			System.out.println(mbSvc.findById(id));
+			System.out.println("** 상세보기 **");
+			System.out.println("수정할 이름 (없으면 엔터): ");
+			String name_2 = sc.nextLine();
+			name_2 = name_2.length() == 0 ? dto.getName() : name_2;
+			System.out.println("수정할 비밀번호 (없으면 엔터): ");
+			String passwd_2 = sc.nextLine();
+			passwd_2 = passwd_2.length() == 0 ? dto.getPasswd() : passwd_2;
+			System.out.println("수정할 피부타입 (없으면 엔터) : ");
+			String skin_type_2 = sc.nextLine();
+			skin_type_2 = skin_type_2.length() == 0 ? dto.getSkintype() : skin_type_2;
+			System.out.println("수정할 나이 (없으면 엔터) : ");
+			String age_2 = sc.nextLine();
+			int age_3 = 0;
+			age_3 = age_2.length() == 0 ? dto.getAge() : Integer.parseInt(age_2);
+			dto.setId(id);
+			dto.setName(name_2);
+			dto.setPasswd(passwd_2);
+			dto.setSkintype(age_2);
+			dto.setAge(age_3);
+			mbSvc.update(dto);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (MemberException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	private void deleteMember() {
 		System.out.println("삭제하고 싶은 회원 아이디를 입력하세요");
 		String id = (sc.nextLine());
 
+		try {
+			mbSvc.delete(id);
+			System.out.println(id + "가 삭제되었습니다.");
+		} catch (MemberException e) {
+			e.printStackTrace();
+		}
 
-			try {
-				mbSvc.delete(id);
-				System.out.println(id+"가 삭제되었습니다.");
-			} catch (MemberException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		
 	}
-	
-	
-	
-	
-	
+
 	private void deleteBoard() {
 		System.out.println("삭제하고 싶은 게시물 번호 입력하세요");
 		int no = Integer.parseInt(sc.nextLine());
@@ -307,7 +285,7 @@ public class pureViewUi {
 		}
 
 	}
-	
+
 	private void updateBoard() {
 		System.out.println("수정할려는 게시물 번호를 입력하세요 >> ");
 		int BoardNum = Integer.parseInt(sc.nextLine());
@@ -352,7 +330,6 @@ public class pureViewUi {
 		try {
 			brdSvc.add(dto);
 		} catch (BoardException e) {
-//			e.printStackTrace();
 			System.out.println("게시물 등록 오류");
 		}
 	}
