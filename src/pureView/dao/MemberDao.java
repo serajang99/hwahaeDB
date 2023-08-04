17a5a73a2608f37f7845d55aaf999a769612b148
student <att815@naver.com>
2023-08-04 15:32:42 +0900

    style: 코드 정리 + umlcd

--- src/pureView/dao/MemberDao.java
+++ src/pureView/dao/MemberDao.java
@@ -6,18 +6,20 @@ import java.sql.SQLException;
 import pureView.dto.MemberDto;
 import pureView.exception.DuplicatedIdException;
 
-import pureView.exception.MemberException;
 import pureView.exception.RecordNotFoundException;
 
-public interface MemberDao{
-	  //등록
-	  public void add(MemberDto m) throws SQLException, DuplicatedIdException;
-	  //수정
-	  public void update(MemberDto m) throws SQLException, RecordNotFoundException;
-	  //삭제
-	  public void delete(String id) throws SQLException, RecordNotFoundException;
-	  //아이디 조회, 비밀번호 조회 
-	  public MemberDto findById(String id) throws SQLException,RecordNotFoundException;
-	  public MemberDto findByPw(String id) throws SQLException,RecordNotFoundException;
-}
+public interface MemberDao {
+	// 등록
+	public void add(MemberDto m) throws SQLException, DuplicatedIdException;
 
+	// 수정
+	public void update(MemberDto m) throws SQLException, RecordNotFoundException;
+
+	// 삭제
+	public void delete(String id) throws SQLException, RecordNotFoundException;
+
+	// 아이디 조회, 비밀번호 조회
+	public MemberDto findById(String id) throws SQLException, RecordNotFoundException;
+
+	public MemberDto findByPw(String id) throws SQLException, RecordNotFoundException;
+}
--- src/pureView/service/LogService.java
+++ src/pureView/service/LogService.java
@@ -9,18 +9,23 @@ import pureView.exception.RecordNotFoundException;
 import pureView.dto.LoginDto;
 
 public interface LogService {
-	  //등록
-	  public void add(LoginDto m) throws LogException;
-	  //수정
-	  public void update_in(LoginDto m) throws LogException,RecordNotFoundException;
-	  public void update_out(LoginDto m) throws LogException,RecordNotFoundException;
-	  //삭제
-	  public void delete(String id) throws LogException;
-	  //갯수
-	  public int count() throws LogException;
-	  //목록
-	  public List<LoginDto> read() throws LogException;
-	  //검색
-	  public LoginDto findById(String id) throws LogException,RecordNotFoundException;;
+	// 등록
+	public void add(LoginDto m) throws LogException;
 
+	// 수정
+	public void update_in(LoginDto m) throws LogException, RecordNotFoundException;
+
+	public void update_out(LoginDto m) throws LogException, RecordNotFoundException;
+
+	// 삭제
+	public void delete(String id) throws LogException;
+
+	// 갯수
+	public int count() throws LogException;
+
+	// 목록
+	public List<LoginDto> read() throws LogException;
+
+	// 검색
+	public LoginDto findById(String id) throws LogException, RecordNotFoundException;
 }
